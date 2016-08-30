cfd150f33b583b2408cef0e57108798b3be98990
Luise Rauch <luise.rauch@gmail.com>
2016-08-30 10:57:15 +0200

    Assignment 7 Services Pt2 completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,17 +2,21 @@
   function SongPlayer() {
     var SongPlayer = {};
 
+    /**
+    * @desc(A short description) selected song object and private attribute
+    * @type(The type, such as {Object}, {Array}, {Number}, etc.) {Object}
+    */
     var currentSong = null;
 
     /**
-    * @desc(A short description) Buzz object audio file
-    * @type(The type, such as {Object}, {Array}, {Number}, etc.) {Object}
+    * @desc Buzz object audio file and private attribute
+    * @type {Object}
     */
     var currentBuzzObject = null;
 
     /**
     * @function(Name of the function) setSong
-    * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+    * @desc A private function stopping the currently playing song and loading the new audio file as currentBuzzObject
     * @param(A list of parameters and their type) {Object} song
     */
     var setSong = function(song) {
@@ -29,14 +33,33 @@
       currentSong = song;
     };
 
+    /**
+    * @function playSong
+    * @desc A private function playing the current song and setting the playing property of the song object to true
+    * @param {Object} song
+    */
+    var playSong = function(song){
+      currentBuzzObject.play();
+      song.playing = true;
+    };
+
+    /**
+    * @function SongPlayer.play
+    * @desc This public method plays the selected song
+    * @param {Object} song
+    */
     SongPlayer.play = function(song) {
       if (currentSong !== song) {
         setSong(song);
-        currentBuzzObject.play();
-        song.playing = true;
+        playSong(song);
       }
     };
 
+    /**
+    * @function SongPlayer.pause
+    * @desc This public method pauses the selected song
+    * @param {Object} song
+    */
     SongPlayer.pause = function(song) {
       currentBuzzObject.pause();
       song.playing = false;
